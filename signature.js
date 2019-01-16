d1f5319dcc3f86991d33b444387f062581ce9d2a
zz <zz634682577@163.com>
2019-01-16 09:40:49 +0800

    DSA籤名算法  增加字節頭協議

--- signature.js
+++ signature.js
@@ -2,14 +2,27 @@
 "use strict";
 var ecdsa = require('secp256k1');
 
+/**
+ * 签名
+ * @param hash
+ * @param priv_key
+ * @returns {string}
+ */
 exports.sign = function(hash, priv_key){
 	var res = ecdsa.sign(hash, priv_key);
-	return res.signature.toString("base64");
+	return 32 + res.signature.toString("base64");
 };
 
+/**
+ * 验证
+ * @param hash
+ * @param b64_sig
+ * @param b64_pub_key
+ * @returns {*}
+ */
 exports.verify = function(hash, b64_sig, b64_pub_key){
 	try{
-		var signature = new Buffer(b64_sig, "base64"); // 64 bytes (32+32)
+		var signature = new Buffer(b64_sig.substring(2), "base64"); // 64 bytes (32+32)
 		return ecdsa.verify(hash, signature, new Buffer(b64_pub_key, "base64"));
 	}
 	catch(e){
@@ -18,3 +31,20 @@ exports.verify = function(hash, b64_sig, b64_pub_key){
 	}
 };
 
+/**
+ * 恢复公钥
+ * @param hash
+ * @param b64_sig
+ * @param recovery
+ * @returns {*}
+ */
+exports.recover = function(hash, b64_sig, recovery){
+	try{
+		var signature = new Buffer(b64_sig.substring(2), "base64");
+		return ecdsa.recover(hash,signature,recovery);
+	}
+	catch(e){
+		console.log('recover publickey exception: '+e.toString());
+		return false;
+	}
+};
