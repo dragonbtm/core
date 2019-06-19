0b8a1b3338dc212209515bae499d15e7782720e5
zhang0135789 <zz634682577@163.com>
2019-06-19 00:03:40 -0700

    init sql

--- initial-db/core-mysql.sql
+++ initial-db/core-mysql.sql
@@ -711,7 +711,7 @@ CREATE TABLE private_profiles (
 	creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	FOREIGN KEY (unit) REFERENCES units(unit)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-CREATE UNIQUE INDEX IF NOT EXISTS unqPayloadHash ON private_profiles(payload_hash);
+CREATE UNIQUE INDEX `unqPayloadHash` ON private_profiles(`payload_hash`);
 
 CREATE TABLE private_profile_fields (
 	private_profile_id INTEGER NOT NULL ,
@@ -784,3 +784,8 @@ CREATE TABLE correspondent_settings (
 	creation_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY (device_address, correspondent_address)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+
+-- init new
+CREATE UNIQUE INDEX  hcobyAddressSpentMci ON headers_commission_outputs(address, is_spent, main_chain_index);
+CREATE UNIQUE INDEX  byWitnessAddressSpentMci ON witnessing_outputs(address, is_spent, main_chain_index);
